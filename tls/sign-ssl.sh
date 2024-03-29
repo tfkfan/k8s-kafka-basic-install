2e143ba1d83e7d2a6f0532768b6e006e21b604e8
tfkfan <sylar131@live.ru>
2024-03-29 16:48:05 +0300

    Little fix

--- tls/sign-ssl.sh
+++ tls/sign-ssl.sh
@@ -4,8 +4,6 @@ validity_days=1200
 ca_path=$1
 keypair_name=$2
 csr_path=$3
-keystore_password=changeit
-truststore_password=changeit
 
 echo "Signing cert.."
 
@@ -19,18 +17,6 @@ openssl x509 \
   -copy_extensions copyall \
   -out ${keypair_name}.crt
 
-rm -f ./${keypair_name}.csr
-
 echo "Cert validation.."
 
 openssl x509 -in  ${keypair_name}.crt -noout -text
-
-openssl pkcs12 -export -in ${keypair_name}.crt -passout pass:"${keystore_password}" -inkey ${keypair_name}.key -out ${keypair_name}.keystore.p12
-
-keytool -keystore ${keypair_name}.truststore.jks -alias caroot -import -file ${ca_path}.crt -storepass "${truststore_password}" -noprompt
-keytool -keystore ${keypair_name}.truststore.jks -alias 1 -import -file ${keypair_name}.crt -storepass "${truststore_password}" -noprompt
-
-keytool -importkeystore -srckeystore ${keypair_name}.keystore.p12 -srcstoretype PKCS12 -srcstorepass "${keystore_password}" \
-          -deststorepass "${keystore_password}" -destkeystore ${keypair_name}.keystore.jks -noprompt
-
-rm -f ${keypair_name}.keystore.p12
